bd29b33874724cb840692260c143c95eebc61e6e
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2023-12-04 11:22:44 +0000

    complete day 1 part 2

--- src/day1/day1.spec.ts
+++ src/day1/day1.spec.ts
@@ -7,30 +7,66 @@ describe("On Day 1", () => {
     ).toBe("142");
   });
 
-  describe("getFirstNumber", () => {
-    it(`Given a string
-        When getFirstNumber is called
-        Then should return first number found in string
-        AND return as Number type
-    `, () => {
-      expect(day1.getFirstNumber("1st")).toBe(1);
-      expect(day1.getFirstNumber("s1t")).toBe(1);
-      expect(day1.getFirstNumber("st1")).toBe(1);
-      expect(day1.getFirstNumber("and2th45ree")).toBe(2);
-      expect(day1.getFirstNumber("finalt3s643t")).toBe(3);
-    });
+  it(`part2 is identity function`, () => {
+    expect(
+      day1.solveForPartTwo(
+        `two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen`
+      )
+    ).toBe("281");
   });
-  describe("getLastNumber", () => {
-    it(`Given a string
-    When getLastNumber is called
-    Then should return first number found in string
-    AND return as Number type
+});
+
+describe("getFirstNumber", () => {
+  it(`Given a string
+      When getFirstNumber is called
+      Then should return first number found in string
+  `, () => {
+    expect(day1.getFirstNumber("1st")).toBe("1");
+    expect(day1.getFirstNumber("s1t")).toBe("1");
+    expect(day1.getFirstNumber("st1")).toBe("1");
+    expect(day1.getFirstNumber("and2th45ree")).toBe("2");
+    expect(day1.getFirstNumber("finalt3s643t")).toBe("3");
+  });
+
+  it(`Given a string with a number written in letters
+  When getFirstNumber is called with part 2 param as true
+  Then should return first number found in string
 `, () => {
-      expect(day1.getLastNumber("1st")).toBe(1);
-      expect(day1.getLastNumber("s1t")).toBe(1);
-      expect(day1.getLastNumber("st1")).toBe(1);
-      expect(day1.getLastNumber("and2th45ree")).toBe(5);
-      expect(day1.getLastNumber("finalt3s643t")).toBe(3);
-    });
+    expect(day1.getFirstNumber("two1nine", true)).toBe("2");
+    expect(day1.getFirstNumber("eightwothree", true)).toBe("8");
+    expect(day1.getFirstNumber("abcone2threexyz", true)).toBe("1");
+    expect(day1.getFirstNumber("xtwone3four", true)).toBe("2");
+    expect(day1.getFirstNumber("4nineeightseven2", true)).toBe("4");
+    expect(day1.getFirstNumber("oneight234", true)).toBe("1");
+  });
+});
+describe("getLastNumber", () => {
+  it(`
+  Given a string
+  When getLastNumber is called
+  Then should return last number found in string
+`, () => {
+    expect(day1.getLastNumber("1st")).toBe("1");
+    expect(day1.getLastNumber("s1t")).toBe("1");
+    expect(day1.getLastNumber("st1")).toBe("1");
+    expect(day1.getLastNumber("and2th45ree")).toBe("5");
+    expect(day1.getLastNumber("finalt3s643t")).toBe("3");
+  });
+
+  it(`Given a string with a number written in letters
+  When getLastNumber is called  with part 2 param as true
+  Then should return last number found in string
+`, () => {
+    expect(day1.getLastNumber("two1nine", true)).toBe("9");
+    expect(day1.getLastNumber("eightwothree", true)).toBe("3");
+    expect(day1.getLastNumber("abcone2threexyz", true)).toBe("3");
+    expect(day1.getLastNumber("xtwone3four", true)).toBe("4");
+    expect(day1.getLastNumber("4nineeightseven2", true)).toBe("2");
+    expect(day1.getLastNumber("zoneight234", true)).toBe("4");
+  });
+  it("special case", () => {
+    expect(day1.getLastNumber("one9rvzvlrnzhbnnknxfiveeightwols", true)).toBe(
+      "2"
+    );
   });
 });
--- src/day1/index.ts
+++ src/day1/index.ts
@@ -1,46 +1,99 @@
 import { Day } from "../day";
 
+const numbersObj = {
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+  five: "5",
+  six: "6",
+  seven: "7",
+  eight: "8",
+  nine: "9",
+};
+
+const numbersObjPart2 = {
+  eno: "1",
+  owt: "2",
+  eerht: "3",
+  ruof: "4",
+  evif: "5",
+  xis: "6",
+  neves: "7",
+  thgie: "8",
+  enin: "9",
+};
+
+type NumbersType = keyof typeof numbersObj;
+type NumbersTypePart2 = keyof typeof numbersObjPart2;
+
+function regexGenerator(obj: { [key: string]: string }) {
+  return new RegExp(Object.keys(obj).join("|"), "gi");
+}
+
+const numberRegExp = regexGenerator(numbersObj);
+const numberRegExpPart2 = regexGenerator(numbersObjPart2);
+
+function isNumber(value: string): value is NumbersType {
+  return Object.keys(numbersObj).includes(value);
+}
+
+function isNumberPart2(value: string): value is NumbersTypePart2 {
+  return Object.keys(numbersObjPart2).includes(value);
+}
+
 class Day1 extends Day {
   constructor() {
     super(1);
   }
 
-  getFirstNumber(input: string): number {
-    return Number(input.split("").find((char) => !isNaN(Number(char))));
+  getFirstNumber(input: string, part2 = false): string | undefined {
+    return part2
+      ? input
+          .replace(numberRegExp, (matched) =>
+            isNumber(matched) ? numbersObj[matched] : matched
+          )
+          .split("")
+          .find((char) => !isNaN(Number(char)))
+      : input.split("").find((char) => !isNaN(Number(char)));
   }
 
-  getLastNumber(input: string): number {
-    return Number(
-      input
-        .split("")
-        .reverse()
-        .find((char) => !isNaN(Number(char)))
-    );
+  getLastNumber(input: string, part2 = false): string | undefined {
+    return part2
+      ? input
+          .split("")
+          .reverse()
+          .join("")
+          .replace(numberRegExpPart2, (matched) =>
+            isNumberPart2(matched) ? numbersObjPart2[matched] : matched
+          )
+          .split("")
+          .find((char) => !isNaN(Number(char)))
+      : input
+          .split("")
+          .reverse()
+          .find((char) => !isNaN(Number(char)));
   }
-
-  solveForPartOne(input: string): string {
+  main(input: string, part2 = false): string {
     const lines = input.split("\n");
     let calibrationValue = 0;
 
     lines.forEach((line) => {
-      const firstNumber = this.getFirstNumber(line);
-      const lastNumber = this.getLastNumber(line);
+      const firstNumber = this.getFirstNumber(line, part2);
+      const lastNumber = this.getLastNumber(line, part2);
       const newNumber = Number(`${firstNumber}${lastNumber}`);
       calibrationValue += newNumber;
-
-      console.log(`
-      line: ${line}
-      FirstNumber: ${firstNumber}
-      lastNumber: ${lastNumber}
-      newNumber: ${newNumber}
-      `);
     });
 
     return String(calibrationValue);
   }
 
+  solveForPartOne(input: string): string {
+    return this.main(input);
+  }
+
   solveForPartTwo(input: string): string {
-    return input;
+    return this.main(input, true);
   }
 }
 
