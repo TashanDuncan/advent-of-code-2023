8706ab3ee9e9c57a340b47eb093bb18bb59f60a6
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2023-12-08 14:40:37 +0000

    day 2 part 1 complete

--- src/day2/day2.spec.ts
+++ src/day2/day2.spec.ts
@@ -1,7 +1,47 @@
-import day2 from './index';
+import day2 from "./index";
 
-describe('On Day 2', () =>{
-    it(`part1 is identity function`, ()=>{
-        expect(day2.solveForPartOne('hello')).toBe('hello');
-    })
+describe("On Day 2", () => {
+  it(`part1 is identity function`, () => {
+    expect(
+      day2.solveForPartOne(
+        "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+      )
+    ).toBe("8");
+  });
+  
+  describe("getGameInfo", () => {
+    it("should parse game info correctly", () => {
+      const expected = [
+        [
+          ["3", "blue"],
+          ["4", "red"],
+        ],
+        [
+          ["1", "red"],
+          ["2", "green"],
+          ["6", "blue"],
+        ],
+        [["2", "green"]],
+      ];
+
+      expect(
+        day2.getGameInfo(
+          "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+        )
+      ).toStrictEqual(expected);
+    });
+  });
+
+  describe("validHand", () => {
+    it("should return true if hand is less than bag value", () => {
+      expect(day2.validHand(["3", "blue"])).toBe(true);
+      expect(day2.validHand(["15", "blue"])).toBe(false);
+
+      expect(day2.validHand(["12", "red"])).toBe(true);
+      expect(day2.validHand(["13", "red"])).toBe(false);
+
+      expect(day2.validHand(["12", "green"])).toBe(true);
+      expect(day2.validHand(["14", "red"])).toBe(false);
+    });
+  });
 });
--- src/day2/index.ts
+++ src/day2/index.ts
@@ -1,18 +1,62 @@
 import { Day } from "../day";
 
+type Hand = [string, keyof typeof Day2.BAG];
+const isValidHand = (value: string[]): value is Hand =>
+  value[1] === "red" || value[1] === "green" || value[1] === "blue";
 class Day2 extends Day {
+  static BAG = {
+    red: 12,
+    green: 13,
+    blue: 14,
+  };
+  getGameInfo(game: string) {
+    return game
+      .replace(/Game \d+: /, "")
+      .replace(/,\s/g, " ")
+      .replace(/;\s/g, ";")
+      .split(";")
+      .map((cubes) => {
+        const hand = cubes.split(" ");
+        const pairs = [];
+        for (let index = 0; index < hand.length; index += 2) {
+          const handValue = [hand[index], hand[index + 1]];
+          pairs.push(handValue);
+        }
+        return pairs;
+      });
+  }
 
-    constructor(){
-        super(2);
-    }
+  validHand(hand: Hand): boolean {
+    const [handValue, color] = hand;
+    return !!(Number(handValue) <= Day2.BAG[color]);
+  }
 
-    solveForPartOne(input: string): string {
-        return input;
-    }
+  constructor() {
+    super(2);
+  }
 
-    solveForPartTwo(input: string): string {
-        return input;
-    }
+  solveForPartOne(input: string): string {
+    let result = 0;
+    input.split("\n").forEach((game, index) => {
+      const gameInfo = this.getGameInfo(game);
+      if (
+        gameInfo.every((hands) => {
+          return hands.every((hand) => {
+            if (isValidHand(hand)) {
+              return this.validHand(hand);
+            }
+          });
+        })
+      ) {
+        result += index + 1;
+      }
+    });
+    return String(result);
+  }
+
+  solveForPartTwo(input: string): string {
+    return input;
+  }
 }
 
-export default new Day2;
+export default new Day2();
