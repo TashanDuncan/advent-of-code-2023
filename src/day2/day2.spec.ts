825bbce10d5c146150efe0030d6dc7525529f1c6
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2023-12-08 15:23:59 +0000

    day 2 part 2 complete

--- src/day2/day2.spec.ts
+++ src/day2/day2.spec.ts
@@ -8,6 +8,13 @@ describe("On Day 2", () => {
       )
     ).toBe("8");
   });
+  it(`part2 is identity function`, () => {
+    expect(
+      day2.solveForPartTwo(
+        "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+      )
+    ).toBe("2286");
+  });
   
   describe("getGameInfo", () => {
     it("should parse game info correctly", () => {
--- src/day2/index.ts
+++ src/day2/index.ts
@@ -14,7 +14,7 @@ class Day2 extends Day {
     return value[1] === "red" || value[1] === "green" || value[1] === "blue";
   }
 
-  private getGameInfo(game: string): Hand[][] {
+  public getGameInfo(game: string): Hand[][] {
     return game
       .replace(/Game \d+: /, "")
       .replace(/,\s/g, " ")
@@ -30,7 +30,7 @@ class Day2 extends Day {
       );
   }
 
-  private validHand([handValue, color]: Hand): boolean {
+  public validHand([handValue, color]: Hand): boolean {
     return Number(handValue) <= Day2.BAG[color];
   }
 
@@ -43,9 +43,7 @@ class Day2 extends Day {
     input.split("\n").forEach((game, index) => {
       const gameInfo = this.getGameInfo(game);
       if (
-        gameInfo.every((hands) =>
-          hands.every((hand) => Day2.isValidHand(hand) && this.validHand(hand))
-        )
+        gameInfo.every((hands) => hands.every((hand) => this.validHand(hand)))
       ) {
         result += index + 1;
       }
@@ -53,9 +51,35 @@ class Day2 extends Day {
     return String(result);
   }
 
+  private calculatePower(highestCubes: Map<BagColor, number>): number {
+    const redValue = highestCubes.get("red") || 0;
+    const greenValue = highestCubes.get("green") || 0;
+    const blueValue = highestCubes.get("blue") || 0;
+    return redValue * greenValue * blueValue;
+  }
+
   solveForPartTwo(input: string): string {
-    // Implement your Part Two solution here
-    return input;
+    let result = 0;
+
+    input.split("\n").forEach((game, index) => {
+      const highestCubes = new Map<BagColor, number>();
+
+      const gameInfo = this.getGameInfo(game);
+      gameInfo.forEach((hands) => {
+        hands.forEach(([handValue, color]) => {
+          const currentHighestColorValue = highestCubes.get(color) || 0;
+          const newValue = Number(handValue);
+
+          if (currentHighestColorValue < newValue) {
+            highestCubes.set(color, newValue);
+          }
+        });
+      });
+
+      result += this.calculatePower(highestCubes);
+    });
+
+    return String(result);
   }
 }
 
