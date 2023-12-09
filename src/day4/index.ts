bf6c669fa231bf75fb96cd196a69f98cf1a9bfe7
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2023-12-09 14:49:21 +0000

    day4 part 1 complete

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -1,18 +1,52 @@
 import { Day } from "../day";
 
 class Day4 extends Day {
+  constructor() {
+    super(4);
+  }
+  checkWins(input: string) {
+    let result = 0;
+    const logic = input
+      .replace(/Card \d+: /g, "")
+      .split("\n")
+      .map((line) =>
+        line
+          .split("|")
+          .map((numbers) => numbers.split(" ").filter((num) => num !== ""))
+      );
+    logic.forEach((card) => {
+      const [winningNums, cardNumbers] = card;
+      let matches = 0;
+      let points = 0;
+      winningNums.forEach((number) => {
+        for (let index = 0; index < cardNumbers.length; index++) {
+          if (number === cardNumbers[index]) {
+            matches++;
+          }
+        }
+      });
 
-    constructor(){
-        super(4);
-    }
+      if (matches > 0) {
+        points = 1;
+      }
+      if (matches > 1) {
+        for (let index = 1; index < matches; index++) {
+          points *= 2;
+        }
+      }
 
-    solveForPartOne(input: string): string {
-        return input;
-    }
+      result += points;
+    });
+    return result;
+  }
 
-    solveForPartTwo(input: string): string {
-        return input;
-    }
+  solveForPartOne(input: string): string {
+    return String(this.checkWins(input));
+  }
+
+  solveForPartTwo(input: string): string {
+    return input;
+  }
 }
 
-export default new Day4;
+export default new Day4();
