838e627916b68b2f82d51ce79cb669cdfa65d43c
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2023-12-09 10:21:09 +0000

    day 3 part 1 complete

--- src/day3/day3.spec.ts
+++ src/day3/day3.spec.ts
@@ -1,7 +1,93 @@
-import day3 from './index';
+import day3 from "./index";
 
-describe('On Day 3', () =>{
-    it(`part1 is identity function`, ()=>{
-        expect(day3.solveForPartOne('hello')).toBe('hello');
-    })
+describe("On Day 3", () => {
+  it(`part1 is identity function`, () => {
+    expect(
+      day3.solveForPartOne(
+        `467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598..`
+      )
+    ).toBe("4361");
+  });
+
+  describe("getNumbers", () => {
+    it("should parse a string and return an object with only numbers and indices", () => {
+      expect(day3.getNumbers("467..114..")).toStrictEqual([
+        { number: "467", index: 0 },
+        { number: "114", index: 5 },
+      ]);
+    });
+  });
+
+  describe("isSymbol", () => {
+    test.each([
+      "!",
+      "@",
+      "#",
+      "$",
+      "%",
+      "^",
+      "&",
+      "*",
+      "(",
+      ")",
+      "-",
+      "+",
+      "=",
+      "{",
+      "}",
+      "[",
+      "]",
+      "|",
+      "\\",
+      ":",
+      ";",
+      '"',
+      "'",
+      "<",
+      ">",
+      ",",
+      "?",
+      "/",
+      "~",
+      "`",
+    ])("should return true for symbol: %s", (char) => {
+      // Act
+      const result = day3.isSymbol(char);
+
+      // Assert
+      expect(result).toBe(true);
+    });
+
+    test.each(["a", "z", "A", "Z", "0", "9", " ", ".", "\t", "\n"])(
+      "should return false for non-symbol(including periods): %s",
+      (char) => {
+        // Act
+        const result = day3.isSymbol(char);
+
+        // Assert
+        expect(result).toBe(false);
+      }
+    );
+
+    describe("isSymbolAdjacent", () => {
+      it("should return true if symbol is adjacent on the same line", () => {
+        expect(
+          day3.isSymbolAdjacent({ number: "114", index: 6 }, "467..*114.")
+        ).toBe(true);
+        expect(
+          day3.isSymbolAdjacent({ number: "114", index: 5 }, "467..114*.")
+        ).toBe(true);
+      });
+
+      it("should return true if symbol is adjacent on the line above", () => {
+        expect(
+          day3.isSymbolAdjacent(
+            { number: "114", index: 5 },
+            "467..114.",
+            ".....*"
+          )
+        ).toBe(true);
+      });
+    });
+  });
 });
--- src/day3/index.ts
+++ src/day3/index.ts
@@ -1,18 +1,87 @@
 import { Day } from "../day";
 
+interface NumbersWithIndices {
+  number: string;
+  index: number;
+}
 class Day3 extends Day {
+  constructor() {
+    super(3);
+  }
+  getNumbers(line: string): NumbersWithIndices[] {
+    const regex = /\d+/g;
+    let match: RegExpExecArray | null;
+    const numbersWithIndices: { number: string; index: number }[] = [];
 
-    constructor(){
-        super(3);
+    while ((match = regex.exec(line)) !== null) {
+      numbersWithIndices.push({
+        number: match[0],
+        index: match.index,
+      });
     }
 
-    solveForPartOne(input: string): string {
-        return input;
-    }
+    console.log(numbersWithIndices);
+    return numbersWithIndices;
+  }
 
-    solveForPartTwo(input: string): string {
-        return input;
+  isSymbol(char: string): boolean {
+    return /[^\w\s\d.]/.test(char);
+  }
+
+  public isSymbolAdjacent(
+    number: NumbersWithIndices,
+    currentLine: string,
+    lineAbove: string = "",
+    lineBelow: string = ""
+  ) {
+    if (
+      this.isSymbol(currentLine[number.index - 1]) ||
+      this.isSymbol(currentLine[number.index + number.number.length])
+    ) {
+      return true;
     }
+    for (
+      let index = number.index - 1;
+      index <= number.index + number.number.length;
+      index++
+    ) {
+      if (this.isSymbol(lineAbove[index]) || this.isSymbol(lineBelow[index])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  solveForPartOne(input: string): string {
+    const validNumbers: number[] = [];
+    const lines = input.split("\n");
+    lines.forEach((line, index) => {
+      const numbers = this.getNumbers(line);
+      console.log(numbers);
+      numbers.forEach((number) => {
+        if (
+          this.isSymbolAdjacent(
+            number,
+            line,
+            lines[index - 1],
+            lines[index + 1]
+          )
+        ) {
+          validNumbers.push(Number(number.number));
+        }
+      });
+    });
+    const initialValue = 0;
+    const result = validNumbers.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      initialValue
+    );
+    return String(result);
+  }
+
+  solveForPartTwo(input: string): string {
+    return input;
+  }
 }
 
-export default new Day3;
+export default new Day3();
